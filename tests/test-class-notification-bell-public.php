af8a084d0bf85a42ac2d6cbb836f1754a128b0a2
Naveen giri <1naveengiri@gmail.com>
2022-08-03 11:40:16 +0530

    Add initial commit for 2.0.0
    - remove unwated/incomplete dot files and phpunit tests ( probably start them fresh in future )
    - change version no in readme to set a milestone for me

--- tests/test-class-notification-bell-public.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Buddy_Notification_Bell_PublicTest extends WP_UnitTestCase {
-
-	function test_get_instance(){
-		$instance = Buddy_Notification_Bell_Public::get_instance();
-		$this->assertNotEmpty($instance);
-	}
-}
